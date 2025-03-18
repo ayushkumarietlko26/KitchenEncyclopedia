49ffd343ae517dc2ce04e28b926a3718a4735324
ayushkumarietlko26 <ayushkumar6feb4@gmail.com>
2025-03-18 13:19:13 +0530

    Fixed build issue

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from React build
+// Serve React build files
 app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.get("*", (req, res) => {
