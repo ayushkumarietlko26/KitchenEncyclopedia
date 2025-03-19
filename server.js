9125c6fc29ae4c620db96978e32e1962366de71b
ayushkumarietlko26 <ayushkumar6feb4@gmail.com>
2025-03-19 18:25:23 +0530

    Fixed deployment issue

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-const express = require("express");
 const path = require("path");
-
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Serve React build files
-app.use(express.static(path.join(__dirname, "client", "build")));
+// Serve React frontend
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
+
